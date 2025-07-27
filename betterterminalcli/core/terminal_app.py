855e94ea8f98055ab9389f816b4a8174447b3537
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-07-27 20:49:52 -0300

    FIX: Start TerminalApp Fixing

--- betterterminalcli/core/terminal_app.py
+++ betterterminalcli/core/terminal_app.py
@@ -15,7 +15,8 @@ class TerminalApp:
         self.widgets.append(widget)
 
     def remove_widget(self, widget:BaseWidget):
-        self.widgets.remove(widget)
+        if widget in self.widgets:
+            self.widgets.remove(widget)
 
     def draw_widgets(self):
         for widget in self.widgets:
@@ -34,30 +35,7 @@ class TerminalApp:
             print(f"Widget {i} : {widget.name}")
 
     def run(self, draw_border: bool = True):
-        self.running = True
-        frame_time = 1 / self.fps if self.fps else 0
-        last = time.perf_counter()
-        self.buffer.clear()
-        self.draw_widgets()
-        try:
-            while self.running:
-                self.buffer.os_clearterminal()
-                now = time.perf_counter()
-                dt = now - last
-                last = now
-                self._update_widgets(dt)
-                
-                self.buffer.render(draw_border=True)
-
-                if frame_time:
-                    sleep_left = frame_time - (time.perf_counter() - now)
-                    if sleep_left > 0:
-                        time.sleep(sleep_left)
-        except KeyboardInterrupt:
-            pass
-        finally:
-            self.stop()
-
+        raise NotImplementedError("Não Existe um Método Run Ainda")
 
     def stop(self):
         self.running = False
