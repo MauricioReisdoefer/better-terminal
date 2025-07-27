1d1f51c177c864e0044a4ce253653d0a8001e53f
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-07-27 00:12:22 -0300

    FEAT: Creating the ScreenBuffer

---
+++ betterterminalcli/core/screen_buffer.py
@@ -0,0 +1,71 @@
+import sys
+import time
+import os
+class ScreenBuffer():
+    """
+    A Tela Principal do Terminal, vai cuidar de toda a renderização dinâmica de caracteres.
+    
+    args:
+        width (int) = Largura da Tela
+        height (int) = Altura da Tela
+        fill (str) = Representa espaço Vazio
+    """
+    def __init__(self, width:int = 70, height:int = 30, fill=" "):
+        self.width = width
+        self.height = height
+        self.fill = fill
+        
+        # Define a Matriz Principal (Screen / Terminal)
+        self.screen = [[self.fill for pixel in range(width)] for pixel in range(height)]
+    
+
+    def os_clearterminal():
+        """
+        Limpa a tela do terminal conforme o sistema operacional.
+        """
+        if os.name == 'nt': 
+            os.system('cls')
+        else:  
+            os.system('clear')
+    
+    def clear(self):
+        """
+        Limpa a tela do terminal, mas sem renderizar.
+        """
+        for y in range(self.height):
+            for x in range(self.width):
+                self.screen[y][x] = self.fill
+                
+    def draw(self, x:int, y:int, text:str):
+        """
+        Desenha um texto na tela, sem renderizar.
+        
+        args:
+            x (int) = Posição Horizontal do Primeiro Caractere do Texto
+            y (int) = Posição Vertical do Primeiro Caractere do Texto
+            text (str) = Texto para ser Desenhado na Tela
+        
+        IMPORTANTE: Caso o texto passe da largura ou altura máxima, ele NÃO SERÁ RENDERIZADO.
+        """
+        for i, char in enumerate(text):
+            if 0 <= x+i < self.width and 0 <= y < self.height:
+                self.screen[y][x+i] = char
+    
+    def render(self):
+        """
+        Renderiza a tela no terminal.
+        """
+        sys.stdout.write("\033[H")
+        sys.stdout.write("\n".join("".join(row) for row in self.screen))
+        sys.stdout.flush()
+        
+    def draw_matrix(self, x:int, y:int, matrix:list[list[str]]):
+        """
+        Desenha uma matriz 2D na tela na posição (x,y).
+        """
+        for dy, row in enumerate(matrix):
+            for dx, char in enumerate(row):
+                absolutex, absolutey = x + dx, y + dy
+                if 0 <= absolutex < self.width and 0 <= absolutey < self.height:
+                    self.screen[absolutey][absolutex] = char
+                
